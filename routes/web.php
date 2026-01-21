cc8b16db595f09ca667e5963a6bb9158000e55c0
ch06vy <147226927+ch06vy@users.noreply.github.com>
2026-01-21 15:33:12 +0530

    init

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutUsController;
+use App\Http\Controllers\CareersController;
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/',[HomeController::class,'index']);
+Route::get('/aboutus',[AboutUsController::class,'aboutusIndex']);
+Route::get('/careers', [CareersController::class, 'index']);
